857253dd9c050ad2dd8bc186d918d699b8ab4790
Trevor Anderson <trevor.p.anderson@uconn.edu>
2021-07-03 02:19:35 -0400

    new code, added escape key as a way to quit the game, started the initializeBoard method, etc. 7/3/21

--- Sudoku/Sudoku Puzzle Solver.py
+++ Sudoku/Sudoku Puzzle Solver.py
@@ -15,32 +15,57 @@ from pygame.locals import (
     K_LEFT,
     K_UP,
     K_DOWN,
+    K_KP_ENTER,
     K_ESCAPE,
     KEYDOWN,
     QUIT,
 )
 
-WHITE = (255,255,255)
+WHITE = (0,0,0)
 
-def drawBackground(self):
-    for i in range(9):
-        for j in range(9):
-            self.square = pygame.Surface((60,60))
-            self.square.fill(WHITE)
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+
+board = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+]
+
+# TODO : get user input to see how many numbers they want to start with given on the board and use that number in line 54 in a 'for' loop to interate through 'board' as many times as the user wants in their input
+
+class Board():
+
+    def __init__(self):
+        pass
+
+    def drawBackground(self): # use pygame.draw.line() to make lines for the board
+        pass
+
+    def initializeBoard(self): # set 0's in board to random numbers between one and 10 so that they can be solved
+        rand_row = random.randint(0,8)
+        rand_col = random.randint(0,8)
+        board[rand_row][rand_col] = random.randint(1,9) # TODO : check if the spot that it randomly chooses already has a number in it (if board[rand_row][rand_col] != 0)
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 running = True
 
 while running:
 
-    drawBackground()
+    screen.fill((WHITE))
 
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
-
-
-SCREEN_WIDTH = 1600
-SCREEN_HEIGHT = 1600
-
-pygame.init()
+        elif event.type == QUIT:
+            running = False
